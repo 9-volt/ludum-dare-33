60d5c912966873e00e68b518006d53df27a8d207
bumbu <bmbalex@gmail.com>
2015-08-24 00:05:50 +0300

    Enlarge game to 800px

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(700, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update })
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update })
   , fish
   , food
   , background
