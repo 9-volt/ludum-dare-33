72922f6345a7b4e72b82783d94e0921bd3c1afb7
bumbu <bmbalex@gmail.com>
2015-08-23 18:58:02 +0300

    Make score text static

--- js/text.js
+++ js/text.js
@@ -7,12 +7,11 @@ function Text(game) {
 
 Text.prototype.setup = function() {
   this._text = this.game.add.bitmapText(this.game.camera.x + this.game.camera.width - 210, 20, 'font','00000', 34);
+  this._text.fixedToCamera = true
 }
 
 Text.prototype.update = function() {
   this.progress += this.game.time.physicsElapsed
-
-  this._text.x = this.game.camera.x + this.game.camera.width - 210
   this._text.text = pad(Math.floor(this.progress), 5) // Progresse in physics seconds
 }
 
